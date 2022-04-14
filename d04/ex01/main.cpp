cceb31fdd03df3fc39ebd137cc446ae2a04472b5
EZmodeApps <75046274+EZmodeApps@users.noreply.github.com>
2022-04-14 03:06:35 +0300

    d04 done

--- d04/ex01/main.cpp
+++ d04/ex01/main.cpp
@@ -9,7 +9,6 @@ int main()
 	const Animal* i = new Cat();
 
 	std::cout << "__________________________________" << std::endl;
-
 	std::cout << j->getType() << "." << std::endl;
 	std::cout << i->getType() << "." << std::endl;
 	i->makeSound();
@@ -21,12 +20,10 @@ int main()
 	Dog	dog;
 
 	std::cout << "__________________________________" << std::endl;
-
 	cat.setIdea(0, "need to sleep.");
 	std::cout << cat.getType() << ": "<< cat.getIdea(0) << std::endl;
 	dog.setIdea(0, "need to eat.");
 	std::cout << dog.getType() << ": "<< dog.getIdea(0) << std::endl;
-
 	std::cout << "__________________________________" << std::endl;
 
 	Animal *animals[10];
