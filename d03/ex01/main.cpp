38504cd9ac41fc498afb90926af57f422c3e99b3
EZmodeApps <75046274+EZmodeApps@users.noreply.github.com>
2022-04-14 04:51:48 +0300

    added d03

---
+++ d03/ex01/main.cpp
@@ -0,0 +1,14 @@
+#include "ClapTrap.h"
+#include "ScavTrap.h"
+
+int	main()
+{
+	ClapTrap clap("Clap_test");
+	ScavTrap scav("Scav_test");
+    
+	clap.attack("Clap_target");
+	clap.beRepaired(10);
+	scav.attack("Scav_target");
+	scav.guardGate();
+	return (0);
+}
